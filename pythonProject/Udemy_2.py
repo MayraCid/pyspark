1fb38ee70262d8ed8694c18b267e75843874041f
MayraCid <oros@ciencias.unam.mx>
2023-01-16 13:31:17 -0600

    Proyecto en pyspark

--- pythonProject/Udemy_2.py
+++ pythonProject/Udemy_2.py
@@ -49,9 +49,8 @@ if __name__ == "__main__":
     Udemy2_DF = spark.read.options(escape='"', header = 'True').option("multiline",'true').schema(schema) \
         .csv("C://Users//mayra.cid//Documents//Repositorios//Udemy_Dataset//Course_info.csv")
 
+    print(Udemy2_DF.printSchema())
     print(f"Execution time read: {time.time() - start_time}")
-
-    #print(Udemy2_DF.printSchema())
     #Udemy2_DF.show(5)
 
     #Cambio del tipo de dato
@@ -61,20 +60,15 @@ if __name__ == "__main__":
         .withColumn("num_comments", col("num_comments").cast('int')) \
         .withColumn("num_lectures", col("num_lectures").cast('int')) \
         .withColumn("content_length_min", col("content_length_min").cast('int'))
-
     print(f"Execution time data chage: {time.time() - start_time}")
 
 
-    #print(Udemy2_DF.printSchema())
-    #Udemy2_DF.show(5)
+    print(Udemy2_DF.printSchema())
 
     #Agregamos la columna de año
     Udemy_DF_year = Udemy2_DF.withColumn('year_published', year(Udemy2_DF.published_time))
-
     print(f"Execution time adding new column: {time.time() - start_time}")
 
-    #print(Udemy_DF_year.printSchema())
-    #Udemy_DF_year.show(5)
 
     #Número de filas
     filas = Udemy_DF_year.count()
@@ -82,7 +76,6 @@ if __name__ == "__main__":
     #Número de columnas
     Columnas = len(Udemy_DF_year.columns)
     print(f"El número de columnas es : {Columnas}")
-
     print(f"Execution time number files and columns: {time.time() - start_time}")
 
     #Vemos cuántos valores nulos hay e cada columna
@@ -92,20 +85,18 @@ if __name__ == "__main__":
                "subcategory", "topic", "language", "course_url", "instructor_name",
                "instructor_url", "year_published"]
     Udemy_DF_year.select([count(when(col(c).isNull(), c)).alias(c) for c in Columns]).show()
-
     print(f"Execution time looking for nulls: {time.time() - start_time}")
+
 
     #Podemos ver los elementos nulos, ya sea por columna o id
     Udemy_DF_year.filter(Udemy_DF_year.instructor_url.isNull()).show(4)
-    #Udemy_DF_year.filter(Udemy_DF_year.id == '205502').show()
-
     print(f"Execution time nulls files: {time.time() - start_time}")
 
-    #Eliminando los datos nulos y definiendo un nuevo DataFrame
-    #También se puede eliminar por columna con definiendo subset
-    New_Udemy_DF = Udemy_DF_year.na.drop()
 
+    #Eliminando los datos nulos y definiendo un nuevo DataFrame
+    New_Udemy_DF = Udemy_DF_year.na.drop()
     print(f"Execution time drop null files: {time.time() - start_time}")
+
 
     #Número de filas
     filas = New_Udemy_DF.count()
@@ -113,13 +104,21 @@ if __name__ == "__main__":
     # Número de columnas
     Columnas = len(New_Udemy_DF.columns)
     print(f"El número de columnas es : {Columnas}")
-
     print(f"Execution time number files and columns: {time.time() - start_time}")
+
+
+    # Datos de algunas columnas
+    Udemy_DF_year.describe(['num_subscribers']).show()
+    Udemy_DF_year.describe(['avg_rating']).show()
+    Udemy_DF_year.describe(['num_reviews']).show()
+    Udemy_DF_year.describe(['num_comments']).show()
+    Udemy_DF_year.describe(['num_lectures']).show()
+    print(f"Execution time for describe: {time.time() - start_time}")
+
 
     #Ahora vemos categorias y cuantos hay por cada una en orden descendiente
     df3 = New_Udemy_DF.groupBy("category").count().orderBy("count", ascending = False)
     df3.show()
-
     print(f"Execution time searching categorys: {time.time() - start_time}")
 
     #Ahora vamos a graficar pero para ello hay que pasar a un RDD
@@ -128,7 +127,6 @@ if __name__ == "__main__":
 
     x_rdd = dfx.rdd.map(lambda x: x[0])
     y_rdd = dfy.rdd.map(lambda x: x[0])
-
     print(f"Execution time transform RDD: {time.time() - start_time}")
 
     x = x_rdd.collect()
@@ -138,15 +136,16 @@ if __name__ == "__main__":
     #plt.ylabel('categorías')
     #plt.barh(x,y)
     #plt.show()
+    #print(f"Execution time fisrt graph: {time.time() - start_time}")
 
-    print(f"Execution time fisrt graph: {time.time() - start_time}")
 
     #Veamos las subcategorías pero solo de la categoria = development!!! esto importantisimo
     df4 = New_Udemy_DF[New_Udemy_DF.category == "Development"]
+    print(f"Execution time searching Development: {time.time() - start_time}")
+
 
     df5 = df4.groupBy("subcategory").count().orderBy("count", ascending=False)
     df5.show()
-
     print(f"Execution time Development: {time.time() - start_time}")
 
     #Veamos la grafica de subcategorias de la categoría development
@@ -156,23 +155,20 @@ if __name__ == "__main__":
     #Ahora vamos a graficar pero para ello hay que pasar a un RDD
     x1_rdd = df1x.rdd.map(lambda x: x[0])
     y1_rdd = df1y.rdd.map(lambda x: x[0])
+    print(f"Execution time transform RDD: {time.time() - start_time}")
 
     x1 = x1_rdd.collect()
     y1 = y1_rdd.collect()
-
-    print(f"Execution time transform RDD: {time.time() - start_time}")
 
     #plt.xlabel('Número de cursos')
     #plt.ylabel('Development')
     #plt.barh(x1,y1)
     #plt.show()
-
-    print(f"Execution time graph 2: {time.time() - start_time}")
+    #print(f"Execution time graph 2: {time.time() - start_time}")
 
     #Número de cursos por año
     df6 = New_Udemy_DF.groupBy("year_published").count().orderBy("year_published", ascending=False)
     df6.show()
-
     print(f"Execution time order year: {time.time() - start_time}")
 
     #Presentamos una gráfica de la cantidad de cursos por año
@@ -182,29 +178,18 @@ if __name__ == "__main__":
     #Vamos a gráficar pero para ello hay que pasar a un RRD
     x2_rdd = dfx2.rdd.map(lambda x: x[0])
     y2_rdd = dfy2.rdd.map(lambda x: x[0])
+    print(f"Execution time transform RDD: {time.time() - start_time}")
 
     x2 = x2_rdd.collect()
     y2 = y2_rdd.collect()
 
-    print(f"Execution time transform in RDD: {time.time() - start_time}")
-
-    plt.xlabel('Año')
-    plt.ylabel('Número de cursos ')
-    plt.plot(x2,y2)
-    plt.show()
-
-    print(f"Execution time: {time.time() - start_time}")
+    #plt.xlabel('Año')
+    #plt.ylabel('Número de cursos ')
+    #plt.plot(x2,y2)
+    #plt.show()
+    #print(f"Execution time: {time.time() - start_time}")
 
 
-    #Notas: el primero solo nos da un query más no un DF para ello ocupo lo que tengo arriba
-    #category = New_Udemy_DF.select("category")\
-     #   .groupBy("category")\
-     #   .count()\
-     #   .orderBy("count", ascending = False)\
-     #   .show()
-
-    #Para ver si algo es un dataframe ocupamos
-    #print(isinstance(df4, DataFrame))
 
 
 
